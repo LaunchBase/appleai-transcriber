f983b958cd26aa410b29f3a73f6de95520654da8
Safra Soymat <safra.soymat@habitatlearn.com>
2025-10-08 10:24:50 +0100

    Initial Commit

---
+++ LaunchCameraTranscriber/LaunchCameraTranscriberApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaunchCameraTranscriberApp.swift
+//  LaunchCameraTranscriber
+//
+//  Created by Safra Soymat on 08/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct LaunchCameraTranscriberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
