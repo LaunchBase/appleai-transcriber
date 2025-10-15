f7dcff004fbc9f52debfe0964cd4b229ff3c096e
safra-j <144016836+safra-j@users.noreply.github.com>
2025-10-15 13:09:20 +0100

    Register custom fonts on app launch
    Added font registration in the app initializer for PPWoodland-Bold, PPWoodland-Ultralight, and Manrope fonts using CoreText.

--- LaunchCameraTranscriber/Views/LaunchCameraTranscriberApp.swift
+++ LaunchCameraTranscriber/Views/LaunchCameraTranscriberApp.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
+import CoreText
 
 @main
 struct LaunchCameraTranscriberApp: App {
+    
+    init() {
+        registerFont(name: "PPWoodland-Bold", ext: "otf")
+        registerFont(name: "PPWoodland-Ultralight", ext: "otf")
+        registerFont(name: "Manrope", ext: "ttf")
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+func registerFont(name: String, ext: String) {
+    if let url = Bundle.main.url(forResource: name, withExtension: ext) {
+        CTFontManagerRegisterFontsForURL(url as CFURL, .process, nil)
+    } else {
+        print("Failed to load font \(name).\(ext)")
     }
 }
